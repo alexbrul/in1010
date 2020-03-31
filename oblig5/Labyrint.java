f55859defd096dbd5d77e4ec9d9cd56e6e57c2bd
alexbrul <alexbrul.jobb@gmail.com>
2020-03-31 16:22:27 +0200

    Skal prøve å kode oblig 5 raskt

---
+++ oblig5/Labyrint.java
@@ -0,0 +1,14 @@
+public class Labyrint{
+	Rute[][] nett;
+	int rader;
+	int rekker;
+
+	public Labyrint(int rader, int rekker){
+		this.nett = new Rute[rader][rekker];
+		this.rader = rader;
+		this.rekker = rekker;
+	}
+
+
+
+}
---
+++ oblig5/Rute.java
@@ -0,0 +1,15 @@
+public class Rute{
+	int rad;
+	int rekke;
+	Labyrint lab;
+
+
+	public Rute(int rad, int rekke, Labyrint lab){
+		this.rad = rad;
+		this.rekke = rekke;
+		this.lab = lab;
+	
+	}
+
+
+}
